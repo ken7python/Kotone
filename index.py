e80c7be174d4013c1c6076e1a0e5dbf0a2549f17
ken7python <active.ken.315@gmail.com>
2025-06-08 15:42:41 +0900

    Version1.2.0

--- index.py
+++ index.py
@@ -1,37 +1,37 @@
-from idlelib.configdialog import changes
-
-import sounddevice as sd
-from pydub.effects import normalize,compress_dynamic_range
-
-import noisereduce as nr
-import librosa
-import soundfile as sf
-from scipy.io.wavfile import write
-from pydub import AudioSegment
 import os
+
+import librosa
+import noisereduce as nr
 import numpy as np
+import sounddevice as sd
+import soundfile as sf
+from pydub import AudioSegment
+from pydub.effects import compress_dynamic_range, normalize
+from scipy.io.wavfile import write
 
 fs = 44100
 channels = 1
 seconds = 5
 recording = []
 
-output_dir = './outputs'
-filename = str(input('Enter filename:'))
-if filename == '':
-    filename = 'output.mp3'
+output_dir = "./outputs"
+filename = str(input("Enter filename:"))
+if filename == "":
+    filename = "output.mp3"
 else:
     root, ext = os.path.splitext(filename)
-    if ext == '':
-        filename += '.mp3'
+    if ext == "":
+        filename += ".mp3"
 
 
 print("Start Recording...(Press Enter to stop)")
 sd.default.samplerate = fs
 sd.default.channels = channels
 
+
 def callback(indata, frames, time, status):
     recording.append(indata.copy())
+
 
 stream = sd.InputStream(callback=callback)
 stream.start()
@@ -44,23 +44,39 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 audio_np = np.concatenate(recording, axis=0)
-write(f'{output_dir}/temp.wav', fs, audio_np)
-print("Finished Recording.")
-audio = AudioSegment.from_wav(f'{output_dir}/temp.wav')
+write(f"{output_dir}/temp.wav", fs, audio_np)
+print("Export Recording.")
 
-print('Normalizing...')
-normalized = normalize(audio)
-normalized.export(f'{output_dir}/normalized.wav',format="mp3")
-
-print('Compresing ...')
-compressed = compress_dynamic_range(normalized)
-compressed.export(f'{output_dir}/compressed.wav',format="wav")
-
-print('Noise Reduced...')
-y, sr = librosa.load(f'{output_dir}/compressed.wav', sr=None)
+print("Noise Reduced...")
+y, sr = librosa.load(f"{output_dir}/temp.wav", sr=None)
 reduced = nr.reduce_noise(y=y, sr=sr)
-sf.write(f'{output_dir}/{filename}', reduced, fs)
+sf.write(f"{output_dir}/noise_reduced.wav", reduced, fs)
 
-os.remove(f'{output_dir}/temp.wav')
-os.remove(f'{output_dir}/normalized.wav')
-os.remove(f'{output_dir}/compressed.wav')
+audio = AudioSegment.from_wav(f"{output_dir}/noise_reduced.wav")
+
+print("Normalizing...")
+normalized = normalize(audio)
+normalized.export(f"{output_dir}/normalized.wav", format="mp3")
+
+print("Compresing ...")
+compressed = compress_dynamic_range(normalized,
+    threshold=-30.0,  # 小さい音まで拾う
+    ratio=6.0,  # 強めの圧縮（通常は2〜4）
+    attack=5,  # 反応スピード（ms）
+    release=50  # 落ち着きに戻る時間（ms）
+)
+compressed.export(f"{output_dir}/compressed.wav", format="wav")
+
+print("fade in and fade out...")
+faded = compressed.fade_in(500).fade_out(650)
+faded.export(f"{output_dir}/faded.wav", format="wav")
+
+result = normalize(faded)
+result.export(f"{output_dir}/{filename}", format="mp3")
+
+print(f"Output saved to {output_dir}/{filename}")
+os.remove(f"{output_dir}/noise_reduced.wav")
+os.remove(f"{output_dir}/temp.wav")
+os.remove(f"{output_dir}/normalized.wav")
+os.remove(f"{output_dir}/faded.wav")
+os.remove(f"{output_dir}/compressed.wav")
