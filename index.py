ac15951918f1f009f7d11df43906c9dcbc68e1a5
ken7python <active.ken.315@gmail.com>
2025-06-08 16:02:02 +0900

    Continuous recording possible

--- index.py
+++ index.py
@@ -11,72 +11,77 @@ from scipy.io.wavfile import write
 
 fs = 44100
 channels = 1
-seconds = 5
-recording = []
 
 output_dir = "./outputs"
-filename = str(input("Enter filename:"))
-if filename == "":
-    filename = "output.mp3"
-else:
-    root, ext = os.path.splitext(filename)
-    if ext == "":
-        filename += ".mp3"
+
+print("----------------")
+while True:
+    recording = []
+    filename = str(input("Enter filename:"))
+    if filename == "":
+        filename = "output.mp3"
+    else:
+        root, ext = os.path.splitext(filename)
+        if ext == "":
+            filename += ".mp3"
+
+    print("----------------")
+
+    print("Start Recording...(Press Enter to stop)")
+    sd.default.samplerate = fs
+    sd.default.channels = channels
 
 
-print("Start Recording...(Press Enter to stop)")
-sd.default.samplerate = fs
-sd.default.channels = channels
+    def callback(indata, frames, time, status):
+        recording.append(indata.copy())
 
 
-def callback(indata, frames, time, status):
-    recording.append(indata.copy())
+    stream = sd.InputStream(callback=callback)
+    stream.start()
 
+    input()
+    stream.stop()
+    stream.close()
 
-stream = sd.InputStream(callback=callback)
-stream.start()
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-input()
-stream.stop()
-stream.close()
+    audio_np = np.concatenate(recording, axis=0)
+    write(f"{output_dir}/temp.wav", fs, audio_np)
+    print("Export Recording.")
 
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+    print("Noise Reduced...")
+    y, sr = librosa.load(f"{output_dir}/temp.wav", sr=None)
+    reduced = nr.reduce_noise(y=y, sr=sr)
+    sf.write(f"{output_dir}/noise_reduced.wav", reduced, fs)
 
-audio_np = np.concatenate(recording, axis=0)
-write(f"{output_dir}/temp.wav", fs, audio_np)
-print("Export Recording.")
+    audio = AudioSegment.from_wav(f"{output_dir}/noise_reduced.wav")
 
-print("Noise Reduced...")
-y, sr = librosa.load(f"{output_dir}/temp.wav", sr=None)
-reduced = nr.reduce_noise(y=y, sr=sr)
-sf.write(f"{output_dir}/noise_reduced.wav", reduced, fs)
+    print("Normalizing...")
+    normalized = normalize(audio)
+    normalized.export(f"{output_dir}/normalized.wav", format="mp3")
 
-audio = AudioSegment.from_wav(f"{output_dir}/noise_reduced.wav")
+    print("Compresing ...")
+    compressed = compress_dynamic_range(normalized,
+        threshold=-30.0,  # 小さい音まで拾う
+        ratio=6.0,  # 強めの圧縮（通常は2〜4）
+        attack=5,  # 反応スピード（ms）
+        release=50  # 落ち着きに戻る時間（ms）
+    )
+    compressed.export(f"{output_dir}/compressed.wav", format="wav")
 
-print("Normalizing...")
-normalized = normalize(audio)
-normalized.export(f"{output_dir}/normalized.wav", format="mp3")
+    print("fade in and fade out...")
+    faded = compressed.fade_in(500).fade_out(650)
+    faded.export(f"{output_dir}/faded.wav", format="wav")
 
-print("Compresing ...")
-compressed = compress_dynamic_range(normalized,
-    threshold=-30.0,  # 小さい音まで拾う
-    ratio=6.0,  # 強めの圧縮（通常は2〜4）
-    attack=5,  # 反応スピード（ms）
-    release=50  # 落ち着きに戻る時間（ms）
-)
-compressed.export(f"{output_dir}/compressed.wav", format="wav")
+    result = normalize(faded)
+    result.export(f"{output_dir}/{filename}", format="mp3")
 
-print("fade in and fade out...")
-faded = compressed.fade_in(500).fade_out(650)
-faded.export(f"{output_dir}/faded.wav", format="wav")
+    print(f"Output saved to {output_dir}/{filename}")
+    os.remove(f"{output_dir}/noise_reduced.wav")
+    os.remove(f"{output_dir}/temp.wav")
+    os.remove(f"{output_dir}/normalized.wav")
+    os.remove(f"{output_dir}/faded.wav")
+    os.remove(f"{output_dir}/compressed.wav")
 
-result = normalize(faded)
-result.export(f"{output_dir}/{filename}", format="mp3")
-
-print(f"Output saved to {output_dir}/{filename}")
-os.remove(f"{output_dir}/noise_reduced.wav")
-os.remove(f"{output_dir}/temp.wav")
-os.remove(f"{output_dir}/normalized.wav")
-os.remove(f"{output_dir}/faded.wav")
-os.remove(f"{output_dir}/compressed.wav")
+    print("----------------")
