ecf08ff5579b0df2f058984f89c419d47d6ef409
ken7python <active.ken.315@gmail.com>
2025-08-09 16:50:12 +0900

    Less Edit Versiongit add .

--- old.py
+++ old.py
@@ -7,7 +7,7 @@ import numpy as np
 import sounddevice as sd
 import soundfile as sf
 from pydub import AudioSegment
-from pydub.effects import normalize
+from pydub.effects import compress_dynamic_range, normalize
 from scipy.io.wavfile import write
 
 fs = 44100
@@ -57,14 +57,32 @@ while True:
     audio = AudioSegment.from_wav(f"{output_dir}/noise_reduced.wav")
 
     print("Normalizing...")
-    audio = AudioSegment.from_wav(f"{output_dir}/noise_reduced.wav")
-    result = normalize(audio)
+    normalized = normalize(audio)
+    normalized.export(f"{output_dir}/normalized.wav", format="mp3")
 
+    print("Compresing ...")
+    compressed = compress_dynamic_range(
+        normalized,
+        threshold=-30.0,  # 小さい音まで拾う
+        ratio=6.0,  # 強めの圧縮（通常は2〜4）
+        attack=5,  # 反応スピード（ms）
+        release=50,  # 落ち着きに戻る時間（ms）
+    )
+    compressed.export(f"{output_dir}/compressed.wav", format="wav")
+
+    print("fade in and fade out...")
+    faded = compressed.fade_in(500).fade_out(650)
+    faded.export(f"{output_dir}/faded.wav", format="wav")
+
+    result = normalize(faded)
     result.export(f"{output_dir}/{filename}", format="mp3")
 
     print(f"Output saved to {output_dir}/{filename}")
-    os.remove(f"{output_dir}/temp.wav")
     os.remove(f"{output_dir}/noise_reduced.wav")
+    os.remove(f"{output_dir}/temp.wav")
+    os.remove(f"{output_dir}/normalized.wav")
+    os.remove(f"{output_dir}/faded.wav")
+    os.remove(f"{output_dir}/compressed.wav")
 
     print("----------------")
 print("Finished.")
